4cf90e528df3cb7d64a3349d73e1e936f847c945
Guyllaume Demers <73708367+gdemers-isi@users.noreply.github.com>
2021-03-10 17:00:58 -0500

    Merge Into Timer Logic

--- UnitySample-Tool-TimerDelegate/Assets/Scripts/Timer.cs
+++ UnitySample-Tool-TimerDelegate/Assets/Scripts/Timer.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class Timer : MonoBehaviour
@@ -30,9 +32,16 @@ public class Timer : MonoBehaviour
 
     private bool isInit = false;
 
+    Stopwatch stopwatch;
+
     private void Awake()
     {
         OnTimerEvent = null;
+        stopwatch = new Stopwatch();
+        //Timer.Instance.AddTimerListener(() => { ResetTimer(); });
+        Timer.Instance.AddTimerListener(() => { StartTime(); });
+        Timer.Instance.AddTimerListener(() => { TimeElapse(3000); });
+        Timer.Instance.AddTimerListener(() => { EndTime(5000); });
     }
 
     private void Update()
@@ -52,5 +61,29 @@ public class Timer : MonoBehaviour
     public void AddTimerListener(OnTimerDel myFunc)
     {
         OnTimerEvent += myFunc;
+    }
+
+    private void ResetTimer()
+    {
+        //// not async function since we want it to delay the callback process so we are sure the timer is reset before the other functions
+        ///  gets processed on the other threads
+        stopwatch.Reset();
+    }
+
+    private async void StartTime()
+    {
+        stopwatch.Start();
+        await Task.Run(() => { UnityEngine.Debug.Log($"START TIME : {stopwatch.ElapsedMilliseconds / 1000}"); });
+    }
+
+    private async void TimeElapse(int timeMs)
+    {
+        await Task.Run(() => { Thread.Sleep(timeMs); UnityEngine.Debug.Log($"TIME NOW : {stopwatch.ElapsedMilliseconds / 1000}"); });
+    }
+
+    private async void EndTime(int timeMs)
+    {
+        await Task.Run(() => { Thread.Sleep(timeMs); UnityEngine.Debug.Log($"END TIME : {stopwatch.ElapsedMilliseconds / 1000}"); });
+        stopwatch.Stop();
     }
 }
