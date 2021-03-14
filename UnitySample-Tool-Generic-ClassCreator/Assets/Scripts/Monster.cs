81ff83569997dbaa588eae5039c260027e4e000c
Guyllaume Demers <73708367+gdemers-isi@users.noreply.github.com>
2021-03-14 18:42:25 -0400

    Update Type Generics - Abilities
    need to fix specific selection cases

--- UnitySample-Tool-Generic-ClassCreator/Assets/Scripts/Monster.cs
+++ UnitySample-Tool-Generic-ClassCreator/Assets/Scripts/Monster.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,4 +6,23 @@ using UnityEngine;
 public class Monster : MonoBehaviour
 {
     public List<Ability> abilities;
+
+    private void Awake()
+    {
+        abilities = new List<Ability>();
+    }
+
+    private void Start()
+    {
+        if (abilities != null)
+            CastSpell();
+    }
+
+    private void CastSpell()
+    {
+        foreach (Ability a in abilities)
+        {
+            a.CastAbility();
+        }
+    }
 }
--- UnitySample-Tool-Generic-ClassCreator/Assets/Scripts/SpawnSpotObject.cs
+++ UnitySample-Tool-Generic-ClassCreator/Assets/Scripts/SpawnSpotObject.cs
@@ -14,7 +14,7 @@ public class SpawnSpotObject : MonoBehaviour
     private void Awake()
     {
         ///// Check first if the string is null or not, instanciate gameobject => monsters on the screen
-        if (!String.IsNullOrEmpty(monster_typeName) && ability_mask != -1)
+        if (!String.IsNullOrEmpty(monster_typeName) && ability_mask != -2)
             InstanciateMonster(monster_typeName, ability_mask);
     }
 
@@ -42,12 +42,15 @@ public class SpawnSpotObject : MonoBehaviour
         ///// Need to retrieve the layers that are set from the Editor GUI to set the abilities of the monster
         ///// PROBLEM => If we do a mixed selection, our layer int value result might also be the value of another layer int
         ///// HOW do we go from mixted values to a stringArr?
-        Debug.Log($"Mask int : {mask}");
+        //Debug.Log($"Mask int : {mask}");
         //Debug.Log($"Log value : {(int)Mathf.Log(mask, 2)}");
-        Debug.Log($"Mask Name : {LayerMask.LayerToName((int)Mathf.Log(mask, 2) + layer_offset)}");
+        //Debug.Log($"Mask Name : {LayerMask.LayerToName((int)Mathf.Log(mask, 2) + layer_offset)}");
         //Debug.Log($"Pow value : {Math.Pow(log, 2)}");
         int log = (int)Mathf.Log(mask, bit_value);
         int pow = (int)Math.Pow(bit_value, log);
+
+        int remains = mask - pow;
+        Type ability_type = Type.GetType(LayerMask.LayerToName(remains + layer_offset));
 
         ///// So we retrieve the mask value of the combine layer => example : mask = 12, log2(12) = (int)3.5 = 3, Pow(3,2) = 9
         ///// mask > 9 => so its a combine layer
@@ -56,7 +59,9 @@ public class SpawnSpotObject : MonoBehaviour
         ///// example 3 values that are not next to each other => mask = 10, log2(10) = (int)3.3 = 3, Pow(3,2) = 9
         if (mask > pow)
             monster.abilities = ProcessCombineLayers(mask, pow);
-
+        else
+            monster.abilities?.Add(System.Activator.CreateInstance(ability_type) as Ability);
+        //Debug.Log($"Abilities Array : {monster.abilities[0]}");
         return monster;
     }
 
@@ -67,9 +72,18 @@ public class SpawnSpotObject : MonoBehaviour
         ///// mask - pow = mask position(big value) => from our last example its 3, which gives 2^3 = 8
         List<Ability> abilities = new List<Ability>();
 
-        Type type = Type.GetType(LayerMask.LayerToName((mask - pow) + layer_offset));
-        Debug.Log($"Layer Retrieve : {type}");
+        int remains = mask - pow;
+        Type type = Type.GetType(LayerMask.LayerToName(remains + layer_offset));
+
+        //Debug.Log($"Layer Retrieve : {type}");
+        abilities.Add(System.Activator.CreateInstance(type) as Ability);
+
         ///// mask - mask position = what is left => 12 -8 = 4 => log2(4) = 2, which is the remainding position we are looking for
+        remains = mask - (int)Math.Pow(bit_value, remains);
+        Type type2 = Type.GetType(LayerMask.LayerToName((int)Mathf.Log(remains, bit_value) + layer_offset));
+
+        //Debug.Log($"Layer Retrieve : {type2}");
+        abilities.Add(System.Activator.CreateInstance(type2) as Ability);
 
         ///// KEEP IN MIND THAT YOU COULD HAVE A MULTIPLE SELECTION OF MORE THAN 2
         return abilities;
