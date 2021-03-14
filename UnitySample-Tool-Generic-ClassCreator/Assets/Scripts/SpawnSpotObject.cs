8260615dd00035dee2e3f8109f1e19b2f3ae411d
Guyllaume Demers <73708367+gdemers-isi@users.noreply.github.com>
2021-03-14 16:13:06 -0400

    Theory Finding mask

--- UnitySample-Tool-Generic-ClassCreator/Assets/Scripts/SpawnSpotObject.cs
+++ UnitySample-Tool-Generic-ClassCreator/Assets/Scripts/SpawnSpotObject.cs
@@ -44,7 +44,26 @@ public class SpawnSpotObject : MonoBehaviour
         Debug.Log($"Mask int : {mask}");
         //Debug.Log($"Log value : {(int)Mathf.Log(mask, 2)}");
         Debug.Log($"Mask Name : {LayerMask.LayerToName((int)Mathf.Log(mask, 2) + layer_offset)}");
+        //Debug.Log($"Pow value : {Math.Pow(log, 2)}");
+        int log = (int)Mathf.Log(mask, 2);
+        int pow = (int)Math.Pow(log, 2);
+
+        ///// So we retrieve the mask value of the combine layer => example : mask = 12, log2(12) = (int)3.5 = 3, Pow(3,2) = 9
+        ///// mask > 9 => so its a combine layer
+        ///// Lets test this with another example to confirm the theory
+        ///// example 2 : mask = 6, log2(6) = (int)2.5 = 2, Pow(2,2) = 4 which in our case is already use and gives lazerbeam ability
+        ///// example 3 values that are not next to each other => mask = 10, log2(10) = (int)3.3 = 3, Pow(3,2) = 9
+        if (mask > pow)
+            ProcessCombineLayers();
 
         return monster;
+    }
+
+    private void ProcessCombineLayers()
+    {
+        ///// How do we process them?
+        ///// Mask values are binary based => 2^0, 2^1, 2^2....
+        ///// mask - pow = mask position(big value) => from our last example its 3, which gives 2^3 = 8
+        ///// mask - mask position = what is left => 12 -8 = 4 => log2(4) = 2, which is the remainding position we are looking for
     }
 }
