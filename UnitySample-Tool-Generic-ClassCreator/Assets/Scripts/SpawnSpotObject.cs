0111ccf24dd12a997823629e073fc1e02334d901
Guyllaume Demers <73708367+gdemers-isi@users.noreply.github.com>
2021-03-14 18:11:24 -0400

    Update Fixed mistake
    Math.Pow was raising the log result to a Power of 2 when we are working on a base 2, which mean it should be Pow(2, logResult)

--- UnitySample-Tool-Generic-ClassCreator/Assets/Scripts/SpawnSpotObject.cs
+++ UnitySample-Tool-Generic-ClassCreator/Assets/Scripts/SpawnSpotObject.cs
@@ -9,6 +9,7 @@ public class SpawnSpotObject : MonoBehaviour
     //public MonsterType monsterType_enum;
     public int ability_mask;
     readonly int layer_offset = 6;
+    readonly int bit_value = 2;
 
     private void Awake()
     {
@@ -45,8 +46,8 @@ public class SpawnSpotObject : MonoBehaviour
         //Debug.Log($"Log value : {(int)Mathf.Log(mask, 2)}");
         Debug.Log($"Mask Name : {LayerMask.LayerToName((int)Mathf.Log(mask, 2) + layer_offset)}");
         //Debug.Log($"Pow value : {Math.Pow(log, 2)}");
-        int log = (int)Mathf.Log(mask, 2);
-        int pow = (int)Math.Pow(log, 2);
+        int log = (int)Mathf.Log(mask, bit_value);
+        int pow = (int)Math.Pow(bit_value, log);
 
         ///// So we retrieve the mask value of the combine layer => example : mask = 12, log2(12) = (int)3.5 = 3, Pow(3,2) = 9
         ///// mask > 9 => so its a combine layer
@@ -54,16 +55,23 @@ public class SpawnSpotObject : MonoBehaviour
         ///// example 2 : mask = 6, log2(6) = (int)2.5 = 2, Pow(2,2) = 4 which in our case is already use and gives lazerbeam ability
         ///// example 3 values that are not next to each other => mask = 10, log2(10) = (int)3.3 = 3, Pow(3,2) = 9
         if (mask > pow)
-            ProcessCombineLayers();
+            monster.abilities = ProcessCombineLayers(mask, pow);
 
         return monster;
     }
 
-    private void ProcessCombineLayers()
+    private List<Ability> ProcessCombineLayers(int mask, int pow)
     {
         ///// How do we process them?
         ///// Mask values are binary based => 2^0, 2^1, 2^2....
         ///// mask - pow = mask position(big value) => from our last example its 3, which gives 2^3 = 8
+        List<Ability> abilities = new List<Ability>();
+
+        Type type = Type.GetType(LayerMask.LayerToName((mask - pow) + layer_offset));
+        Debug.Log($"Layer Retrieve : {type}");
         ///// mask - mask position = what is left => 12 -8 = 4 => log2(4) = 2, which is the remainding position we are looking for
+
+        ///// KEEP IN MIND THAT YOU COULD HAVE A MULTIPLE SELECTION OF MORE THAN 2
+        return abilities;
     }
 }
