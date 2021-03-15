e58a271c483a61ad8ee4be4545eb62fcb8351ec1
Guyllaume Demers <73708367+gdemers-isi@users.noreply.github.com>
2021-03-14 20:00:01 -0400

    Update Type Generics
    StackOverflow Exception, calling recursivly works and add the proper skill inside the List<> , still have to test diff. scenario and fix the stackoverflow exception

--- UnitySample-Tool-Generic-ClassCreator/Assets/Scripts/SpawnSpotObject.cs
+++ UnitySample-Tool-Generic-ClassCreator/Assets/Scripts/SpawnSpotObject.cs
@@ -37,55 +37,45 @@ public class SpawnSpotObject : MonoBehaviour
         go.transform.position = transform.position;
 
         Type monster_type = Type.GetType(type);
-
         Monster monster = go.AddComponent(monster_type) as Monster;
-        ///// Need to retrieve the layers that are set from the Editor GUI to set the abilities of the monster
-        ///// PROBLEM => If we do a mixed selection, our layer int value result might also be the value of another layer int
-        ///// HOW do we go from mixted values to a stringArr?
-        //Debug.Log($"Mask int : {mask}");
-        //Debug.Log($"Log value : {(int)Mathf.Log(mask, 2)}");
-        //Debug.Log($"Mask Name : {LayerMask.LayerToName((int)Mathf.Log(mask, 2) + layer_offset)}");
-        //Debug.Log($"Pow value : {Math.Pow(log, 2)}");
+
         int log = (int)Mathf.Log(mask, bit_value);
-        int pow = (int)Math.Pow(bit_value, log);
-
-        int remains = mask - pow;
-        Type ability_type = Type.GetType(LayerMask.LayerToName(remains + layer_offset));
-
-        ///// So we retrieve the mask value of the combine layer => example : mask = 12, log2(12) = (int)3.5 = 3, Pow(3,2) = 9
-        ///// mask > 9 => so its a combine layer
-        ///// Lets test this with another example to confirm the theory
-        ///// example 2 : mask = 6, log2(6) = (int)2.5 = 2, Pow(2,2) = 4 which in our case is already use and gives lazerbeam ability
-        ///// example 3 values that are not next to each other => mask = 10, log2(10) = (int)3.3 = 3, Pow(3,2) = 9
+        int pow = (int)Mathf.Pow(bit_value, log);
         if (mask > pow)
-            monster.abilities = ProcessCombineLayers(mask, pow);
+            ProcessCombineLayers(mask, pow, monster.abilities);
         else
-            monster.abilities?.Add(System.Activator.CreateInstance(ability_type) as Ability);
-        //Debug.Log($"Abilities Array : {monster.abilities[0]}");
+            ProcessSingleLayer(mask, pow, monster.abilities);
+        Debug.Log(monster.abilities.Count);
         return monster;
     }
 
-    private List<Ability> ProcessCombineLayers(int mask, int pow)
+    private void ProcessCombineLayers(int mask, int pow, List<Ability> abilities)
     {
-        ///// How do we process them?
-        ///// Mask values are binary based => 2^0, 2^1, 2^2....
-        ///// mask - pow = mask position(big value) => from our last example its 3, which gives 2^3 = 8
-        List<Ability> abilities = new List<Ability>();
+        try
+        {
+            int remains = mask - pow;
+            if (remains < 0)
+                return;
 
-        int remains = mask - pow;
-        Type type = Type.GetType(LayerMask.LayerToName(remains + layer_offset));
+            Type type = Type.GetType(LayerMask.LayerToName(remains + layer_offset));
+            if (type != null)
+                abilities?.Add(System.Activator.CreateInstance(type) as Ability);
 
-        //Debug.Log($"Layer Retrieve : {type}");
-        abilities.Add(System.Activator.CreateInstance(type) as Ability);
+            int newLog = (int)Mathf.Log(remains, bit_value);
+            int newPow = (int)Mathf.Pow(bit_value, newLog);
 
-        ///// mask - mask position = what is left => 12 -8 = 4 => log2(4) = 2, which is the remainding position we are looking for
-        remains = mask - (int)Math.Pow(bit_value, remains);
-        Type type2 = Type.GetType(LayerMask.LayerToName((int)Mathf.Log(remains, bit_value) + layer_offset));
+            ProcessCombineLayers(remains, newPow, abilities);
+        }
+        catch (System.StackOverflowException e)
+        {
+            Debug.Log("StackOverflow Exception" + e.Message);
+        }
+    }
 
-        //Debug.Log($"Layer Retrieve : {type2}");
-        abilities.Add(System.Activator.CreateInstance(type2) as Ability);
-
-        ///// KEEP IN MIND THAT YOU COULD HAVE A MULTIPLE SELECTION OF MORE THAN 2
-        return abilities;
+    private void ProcessSingleLayer(int mask, int pow, List<Ability> abilities)
+    {
+        Type type = Type.GetType(LayerMask.LayerToName((mask - pow) + layer_offset));
+        if (type != null)
+            abilities?.Add(System.Activator.CreateInstance(type) as Ability);
     }
 }
